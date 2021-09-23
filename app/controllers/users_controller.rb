abce5ae85d9ca6f4cf14e7f40587ec7efc9807a0
chinamiii <chinami.2929@icloud.com>
2021-09-23 13:58:36 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,18 @@ class UsersController < ApplicationController
     @book_today = Book.where(created_at: Date.today.all_day).count
         # bookの1日の登録数
   end
-
+  
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
+    end
+  end
 
 
   def index
